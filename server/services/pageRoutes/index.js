1d1fe2efea39b814aec91817d905b683d7543dd9
Swapnil <pawarswapnil3305@gmail.com>
2025-06-30 19:15:26 +0530

    frontend url check3

--- server/services/pageRoutes/index.js
+++ server/services/pageRoutes/index.js
@@ -2,17 +2,21 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../../models/PostModel");
 
-router.get("/", async (req, res) => {
-  try {
-    let allPosts = await Post.find({}).sort({ createdAt: -1 }).populate({
-      path: "author",
-      select: "username profileImg",
-    });
+// router.get("/", async (req, res) => {
+//   try {
+//     let allPosts = await Post.find({}).sort({ createdAt: -1 }).populate({
+//       path: "author",
+//       select: "username profileImg",
+//     });
 
-    res.json(allPosts);
-  } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
-  }
+//     res.json(allPosts);
+//   } catch (error) {
+//     res.status(500).json({ message: "Internal Server Error" });
+//   }
+// });
+
+router.get("/", async (req, res) => {
+  res.json({ status: "ok" });
 });
 
 module.exports = router;
